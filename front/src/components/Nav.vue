6d5339d959059efc22eec6950e9872fc168ac850
Shithappe <greenmad.b50@gmail.com>
2021-11-29 00:07:37 +0300

    auth

---
+++ front/src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+        <v-container >
+            <v-app-bar
+            color="green"
+        >  
+    
+        </v-app-bar>
+        </v-container>
+        
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router.js";
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router,
+  vuetify,
+  router
 }).$mount('#app')
 
 
