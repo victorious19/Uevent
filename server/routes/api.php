d7f81b14cf46dadcc1744411f459537b542e8ce9
Zapalexa <alexeyzaporozchenko@gmail.com>
2021-11-28 14:36:30 +0200

    routes info

--- server/routes/api.php
+++ server/routes/api.php
@@ -14,36 +14,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//ДЛЯ НЕАВТОРИЗИРОВАННЫХ
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::group(['prefix' => 'auth'], function() {
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('login', 'App\Http\Controllers\AuthController@login');         //авторизация пользователя
+    Route::post('register', 'App\Http\Controllers\AuthController@register');   //регистрация пользователя
 });
 Route::group(['prefix' => 'company'], function() {
-    Route::get('{company_id}', 'App\Http\Controllers\CompanyController@get');
+    Route::get('{company_id}', 'App\Http\Controllers\CompanyController@get');  //инфа о компании по id
 });
 Route::group(['prefix' => 'theme'], function() {
-    Route::get('', 'App\Http\Controllers\ThemeController@get_all');
+    Route::get('', 'App\Http\Controllers\ThemeController@get_all');            //все темы
 });
 Route::group(['prefix' => 'events'], function() {
-    Route::get('', 'App\Http\Controllers\EventController@get_all');
+    Route::get('', 'App\Http\Controllers\EventController@get_all');            //все события
 });
 
+
+//ДЛЯ АВТОРИЗИРОВАННЫХ
 Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::group(['prefix' => 'events'], function() {
-        Route::patch('{event_id}', 'App\Http\Controllers\EventController@change');
-        Route::delete('{event_id}', 'App\Http\Controllers\EventController@delete');
+        Route::patch('{event_id}', 'App\Http\Controllers\EventController@change');   //изменить событие
+        Route::delete('{event_id}', 'App\Http\Controllers\EventController@delete');  //удалить событие
     });
     Route::group(['prefix' => 'theme'], function() {
-        Route::post('', 'App\Http\Controllers\ThemeController@create');
-        Route::delete('{theme_id}', 'App\Http\Controllers\ThemeController@create');
+        Route::post('', 'App\Http\Controllers\ThemeController@create');              //создать тему
+        Route::delete('{theme_id}', 'App\Http\Controllers\ThemeController@create');  //удалить тему
     });
     Route::group(['prefix' => 'company'], function() {
-        Route::post('', 'App\Http\Controllers\CompanyController@create');
-        Route::patch('{company_id}', 'App\Http\Controllers\CompanyController@change');
-        Route::delete('{company_id}', 'App\Http\Controllers\CompanyController@delete');
+        Route::post('', 'App\Http\Controllers\CompanyController@create');               //создать компанию
+        Route::patch('{company_id}', 'App\Http\Controllers\CompanyController@change');  //изменить компанию
+        Route::delete('{company_id}', 'App\Http\Controllers\CompanyController@delete'); //удалить компанию
     });
 });
