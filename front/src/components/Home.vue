ab0d5e088bd92e313ad4ab83f17f1b574bbb07c0
victorious19 <75379661+victorious19@users.noreply.github.com>
2021-11-29 14:07:39 +0200

    fix

---
+++ front/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>home</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi libero nulla autem culpa expedita illo accusamus voluptas ea sit eaque? Assumenda esse nesciunt nostrum animi, ab delectus ut labore repellat?</p>
+        <h1>[eqyz</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
