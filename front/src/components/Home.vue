2e41e61685f4c9d06d768ed051545f5b2766ddb7
Shithappe <greenmad.b50@gmail.com>
2021-11-29 20:52:20 +0300

    all events

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,7 +1,72 @@
 <template>
     <div>
-        <h1>home</h1>
-        <h1>[eqyz</h1>
+        <div v-for="event in events" :key="event.id">
+    <!-- <v-card
+      class="mx-auto"
+      max-width="344"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="text-overline mb-4">
+            OVERLINE
+          </div>
+          <v-list-item-title class="text-h5 mb-1">
+            Headline 5
+          </v-list-item-title>
+          <v-list-item-subtitle>{{event.location}}</v-list-item-subtitle>
+        </v-list-item-content>
+  
+        <v-list-item-avatar
+          tile
+          size="80"
+          color="grey"
+        ></v-list-item-avatar>
+      </v-list-item>
+  
+      <v-card-actions>
+        <v-btn
+          outlined
+          rounded
+          text
+        >
+          Button
+        </v-btn>
+      </v-card-actions>
+    </v-card> -->
+
+    <!--  -->
+
+    <v-card
+      class="mx-auto mb-4"
+      max-width="640"
+    >
+      <v-card-text>
+        <div>Word of the Day</div>
+        <p class="text-h4 text--primary">
+          "name"
+          <img src="" alt="{event.poster}">
+        </p>
+        <p>{{event.format}}</p>
+        <div class="text--primary">
+         {{event.description}}
+        </div>
+        <div class="text">
+            {{event.date}}
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          color="deep-purple accent-4"
+        >
+          Learn More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+        </div>
+
     </div>
 </template>
 
